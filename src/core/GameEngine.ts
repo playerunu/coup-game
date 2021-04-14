69ad696f48917616a153f524c001e3160e7e9df1
Mihaita Leoveanu <playerunu@gmail.com>
2021-04-15 01:12:39 +0300

    WIP on merging game state from messageUpdates

--- src/core/GameEngine.ts
+++ src/core/GameEngine.ts
@@ -1,12 +1,67 @@
 import {Game} from "../model/Game";
+import {Card} from "../model/Card";
+import {Player} from "../model/Player";
+import {keyToString} from "../utils/keyToString";
+import {deepMerge} from "../utils/deepMerge";
 
 export class GameEngine {
     // Intial game state is empty, it will be populated by incremental updates from the back-end
     game: Game = { 
         players: [],
+        currentPlayer: {},
+        playerActions: [],
+        tableCoins: 0,
     }
 
     public heroPlayerName: string;
+
+    updateGame(source: any) {
+        let game = this.game;
+        for (const key in source) {
+            // We use keyToString helper function here in order to force
+            // a strongly-typed check on each of the game keys 
+            switch(key) {
+                case keyToString(game, game.players):
+
+                    break;
+                case keyToString(game, game.currentPlayer):
+                    deepMerge(game.currentPlayer, source[key]);
+                    break;
+                case keyToString(game, game.playerActions):
+                    break;
+                case keyToString(game, game.tableCoins):
+                    break;
+
+            }
+        }
+        return 0;
+    }
+    
+    setHeroPlayerCards(cards: Card[]) {
+        // Lookup if the player already exists in game state
+        let heroPlayer: Player = null;
+
+        for (const player of this.game.players) {
+            if (this.isHeroPlayer(player)){
+                heroPlayer = player;
+                break;
+            }
+        }
+
+        // Player not found, create it 
+        if (heroPlayer == null){
+            heroPlayer = {
+                name: this.heroPlayerName,
+                cards: []
+            }
+        }
+
+        heroPlayer.cards = cards;
+    }
+
+    isHeroPlayer(player: Player){
+        return player.name == this.heroPlayerName;
+    }
 }
 
 export const engine= new GameEngine();
---
+++ src/utils/keyToString.ts
@@ -0,0 +1,8 @@
+export function keyToString(object, value){
+    for (let objKey in object) {
+        if (object[objKey] == value) {
+            return objKey;
+        }
+    }
+    return false;
+}
